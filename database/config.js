36f21067bc28adc3e6cccf92f70244bce27e6e68
DaniRicher <richerdani3@gmail.com>
2022-07-06 15:27:59 -0500

    Cors y express

---
+++ database/config.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+
+        await mongoose.connect( process.env.DB_CNN );
+        console.log('DB Online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al inciar la base de datos');
+    }
+
+}
+
+
+module.exports = {
+    dbConnection
+}
+
+
