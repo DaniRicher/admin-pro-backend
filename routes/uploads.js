14d0871ebc7cbbf16004f7a64e95e499e0cabe37
DaniRicher <richerdani3@gmail.com>
2022-07-10 17:14:45 -0500

    Subir una imagen

---
+++ routes/uploads.js
@@ -0,0 +1,22 @@
+/*
+    Ruta: api/uploads/
+*/
+
+const { Router } = require('express');
+const expressFileUpload = require('express-fileupload');
+const { fileUpload } = require('../controllers/uploads');
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+const router = Router();
+
+router.use( expressFileUpload() );
+
+router.put( '/:tipo/:id',[
+    validarJWT
+], fileUpload );
+
+
+
+module.exports= router;
