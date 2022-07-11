eade92e4fb1ca58f7d4bebafb920145ddb70998f
DaniRicher <richerdani3@gmail.com>
2022-07-11 15:12:59 -0500

    Seccion 12 Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const { response } = require("express");
+const { googleVerify } = require("../helpers/google-verify");
+const { generarJWT } = require("../helpers/jwt");
 const bcrypt = require('bcrypt');
 const Usuario = require("../models/usuario");
-const { generarJWT } = require("../helpers/jwt");
 
 
 const login = async(req, res = response) => {
@@ -47,6 +48,53 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response) => {
+
+    try {
+
+        const { email, name, picture } = await googleVerify( req.body.token );
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if( !usuarioDB ) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        //Guardar Usuario
+        await usuario.save();
+
+        //Generar TOKEN - JWT
+        const token = await  generarJWT( usuarioDB.id );
+
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        });
+        
+    } catch (error) {
+
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'Token de google no es correcto'
+        });
+    }
+
+}
+
+
 module.exports = {
     login,
+    googleSignIn
 }
