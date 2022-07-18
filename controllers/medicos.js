087c79b4468e72b99a53a0dc864801d436a4d364
DaniRicher <richerdani3@gmail.com>
2022-07-18 01:17:07 -0500

    Fin sección 17 - Get medicoById

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -15,6 +15,31 @@ const getMedicos = async( req, res= response ) => {
     });
 
 }
+const getMedicoById = async( req, res= response ) => {
+    
+    const id = req.params.id;
+
+    try {
+        const medico = await Medico.findById( id )
+                                .populate('usuario', 'nombre img')
+                                .populate('hospital', 'nombre img');
+    
+        res.json({
+            ok:true,
+            medico
+        });
+        
+    } catch (error) {
+
+        console.log(error);
+        return res.json({
+            ok: false,
+            msg:'Medico no encontrado'
+        });
+    }
+    
+
+}
 
 const crearMedico = async ( req, res= response ) => {
 
@@ -117,5 +142,6 @@ module.exports = {
     getMedicos,
     crearMedico,
     actualizarMedico,
-    borrarMedico
+    borrarMedico,
+    getMedicoById
 }
